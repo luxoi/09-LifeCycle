9d8aaeca2bdc5a71dd2230a94876f448a64382a3
Luis Loyola Cuevas <luis.loyola2411@gmail.com>
2024-10-15 15:19:40 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './products/pages/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  children: [
+    { path: 'product', component: ProductComponent },
+    { path: '**', redirectTo: 'product' },
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'products-product-price',
+  templateUrl: './price.component.html',
+  styleUrl: './price.component.css'
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0;
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('Componente hijo ngOnInit');
+
+    this.interval$ = interval(1000).subscribe(value => console.log(`Tick: ${value} `))
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    console.log('Componente hijo ngOnChanges')
+    console.log(changes);
+
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente hijo ngOnDestroy')
+    this.interval$?.unsubscribe();
+  }
+
+
+
+}
